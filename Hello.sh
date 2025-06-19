ef5ab2069310cc7dec691596ef470a92a425ece8
Gourav-patil-git <gouravpatil654@gmail.com>
2025-06-19 14:58:39 +0530

    small change

--- Hello.sh
+++ Hello.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "Hello I am Gourav this is my first shell script"
+echo "Hello I am DevOps asperent this is my first shell script"
 
 echo "Today I am Start learning Shell Script at:- $(date)"
