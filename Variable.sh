182f0be5127954c9ed97e6beb4b98cd151f4f134
Gourav-patil-git <gouravpatil654@gmail.com>
2025-06-19 15:11:34 +0530

    small change

--- Variable.sh
+++ Variable.sh
@@ -6,3 +6,13 @@ echo "Enter your name: "
 read name
 
 echo "Your Name is: $name"
+
+
+# using Readonly 
+# you can not change valu of variable once its declared as readonly
+
+let NAME=sagar
+readonly NAME
+NAME=DEVOPS
+echo "my name is: $NAME"
+
