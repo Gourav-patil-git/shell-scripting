b0e1a282ce8bcb9d50c590b66ec90c416e81f352
Gourav-patil-git <gouravpatil654@gmail.com>
2025-06-20 17:07:06 +0530

    Spetial Variable

---
+++ SpetialVariable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# test of few of the fixed variables
+echo "filename: $0"
+echo "1st param: $1"
+echo "2nd param: $2"
+echo "quoted value: $@"
+echo "quoted vablue : $*"
+echo "no of params: $#"
